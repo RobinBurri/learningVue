9d8fc34599a5ad5bd0351df67cf9722e9d814344
Robin Burri <robinburri@tutanota.com>
2025-05-05 09:55:03 +0200

    add navbar

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,34 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import TheHeader from './components/layout/TheHeader.vue'
+</script>
 
 <template>
-  <main></main>
+  <TheHeader />
+  <router-view />
 </template>
 
-<style scoped></style>
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap');
+
+:root {
+  --color-primary: #008e4f;
+  --color-primary-light: #00b36d;
+  --color-primary-dark: #006a3d;
+  --color-accent: #ff2058;
+  --color-accent-light: #ff4c7f;
+  --color-accent-dark: #c7003a;
+  --color-white: #ffffff;
+}
+
+* {
+  box-sizing: border-box;
+}
+
+html {
+  font-family: 'Roboto', sans-serif;
+}
+
+body {
+  margin: 0;
+}
+</style>
---
+++ src/pages/coaches/CoachesList.vue
@@ -0,0 +1,15 @@
+<template>
+  <section>Filters</section>
+  <div>
+    <h1>Coaches List</h1>
+    <ul>
+      <li v-for="coach in coaches" :key="coach.id">
+        {{ coach.name }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script lang="ts" setup></script>
+
+<style scoped></style>
